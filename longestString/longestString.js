30d69c3ca54f25d1530c9b0cb59b868c47e608cd
BigLipBandit <yourfavoreo@gmail.com>
2017-08-08 18:22:02 -0500

    longestString

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,4 +1,15 @@
-/*
- * Write a function that accepts an array of strings.
- * Return the longest string in the array.
- */
+const getLongestString = (arr) => {
+  let fruitName =[];
+  let lngth = 0; 
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > lngth) {
+      lngth = arr[i].length;
+      fruitName = arr[i];
+    }
+  }
+
+  return fruitName;
+}
+//const output = getLongestString(['peach','grapes','grapefruit']);
+console.log(getLongestString(['peach','grapefruits','grapefruit']));
